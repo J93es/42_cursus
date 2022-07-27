e070192cc5034344679ae2ed0e47daabb7927e5b
mijung <tv0924@icloud.com>
2022-07-27 15:04:49 +0900

    7/27.3

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mijung <mijung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 11:52:01 by mijung            #+#    #+#             */
-/*   Updated: 2022/07/09 18:21:08 by mijung           ###   ########.fr       */
+/*   Updated: 2022/07/27 14:49:32 by mijung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,20 @@
 char	*ft_strdup(const char *s1)
 {
 	size_t	s1_len;
-	char	*dest;
+	char	*dst;
 	char	*ptr;
+	char	*s1_temp;
 
 	s1_len = 0;
-	while (*(s1 + s1_len) != 0)
+	s1_temp = (char *)s1;
+	while (*(s1_temp + s1_len) != 0)
 		s1_len++;
-	dest = (char *)malloc(s1_len + 1);
-	if (dest == 0)
+	dst = (char *)malloc(s1_len + 1);
+	if (dst == 0)
 		return (0);
-	ptr = dest;
-	while (*s1 != 0)
-		*ptr++ = *s1++;
+	ptr = dst;
+	while (*s1_temp != 0)
+		*ptr++ = *s1_temp++;
 	*ptr = 0;
-	return (dest);
+	return (dst);
 }
--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mijung <mijung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 11:52:47 by mijung            #+#    #+#             */
-/*   Updated: 2022/07/09 11:53:32 by mijung           ###   ########.fr       */
+/*   Updated: 2022/07/27 14:55:30 by mijung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	char	*src_temp;
 	size_t	i;
 	size_t	src_len;
 	size_t	dst_len;
@@ -21,15 +22,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	src_len = 0;
 	dst_len = 0;
-	while (*(src + src_len) != 0)
+	src_temp = (char *)src;
+	while (*(src_temp + src_len) != 0)
 		src_len++;
 	while (*(dst + dst_len) != 0)
 		dst_len++;
 	if (dstsize <= dst_len)
 		return (src_len + dstsize);
-	while (*(src + i) != 0 && dstsize > dst_len + i + 1)
+	while (*(src_temp + i) != 0 && dstsize > dst_len + i + 1)
 	{
-		*(dst + dst_len + i) = *(src + i);
+		*(dst + dst_len + i) = *(src_temp + i);
 		i++;
 	}
 	*(dst + dst_len + i) = 0;
